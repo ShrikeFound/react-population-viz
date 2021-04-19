6c4c901d918ce0a5c84fcb3456d75c9b2b1f4a56
Alfonso Martinez <61892420+ShrikeFound@users.noreply.github.com>
2021-04-19 15:18:58 -0700

    init commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { csv } from "d3-fetch";
+import { useEffect, useState } from "react";
+import ScatterPlot from "./components/ScatterPlot";
+
 
 function App() {
+  const [data, setData] = useState();
+
+  const dataURL = "https://gist.githubusercontent.com/ShrikeFound/39a8fd3db574ec9f5d10074840c098bd/raw/ad28b17d52c8e477f02502652ce551dac7c4f280/UN%2520population%2520data.csv"
+
+  console.log(data)
+
+  useEffect(() => {
+  csv(dataURL).then(setData)    
+  }, [])
+  
+  if (!data) {
+    return <p>Loading...</p>
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ScatterPlot data={data}/>
       </header>
     </div>
   );
---
+++ src/components/ScatterPlot.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const ScatterPlot = ({ data }) => {
+ 
+
+  if (!data) {
+    return <p>Loading...</p>
+  }
+
+  return (
+    <div>
+      ScatterPlot
+    </div>
+  )
+}
+
+export default ScatterPlot
